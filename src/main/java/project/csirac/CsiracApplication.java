487d6d2a40b2dd0a7ce01c5f462b62fdfa3330a9
DongyuZhao <dy.zhao@outlook.com>
2016-01-02 16:22:33 +1100

    Submit For Jar Settings

---
+++ src/main/java/project/csirac/CsiracApplication.java
@@ -0,0 +1,12 @@
+package project.csirac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsiracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsiracApplication.class, args);
+	}
+}
---
+++ src/test/java/project/csirac/CsiracApplicationTests.java
@@ -0,0 +1,18 @@
+package project.csirac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CsiracApplication.class)
+@WebAppConfiguration
+public class CsiracApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
