210f362521049b126c51ec9de7392e0f7ca2ef7d
Tessa1994 <cathyorcallmexuan@gmail.com>
2016-01-14 20:39:16 +1100

    add apache common-lang dependency and use StringUtils instead of the StringHelper class

--- src/main/java/project/csirac/CsiracApplication.java
+++ src/main/java/project/csirac/CsiracApplication.java
@@ -1,29 +1,28 @@
 package project.csirac;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import project.csirac.helper.StringHelper;
-import project.csirac.models.emulator.IMonitor;
-import project.csirac.models.emulator.Monitor;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import project.csirac.models.emulator.IMonitor;
+import project.csirac.models.emulator.Monitor;
 
 @SpringBootApplication
 public class CsiracApplication {
 
-    public static IMonitor monitor = new Monitor();
+	public static IMonitor monitor = new Monitor();
 
 	public static Map<String, Long> sessionList = new HashMap<>();
 
-    public static  boolean sessionExists(String sessionId)
-    {
-        if (StringHelper.isNullOrWhiteSpace(sessionId))
-        {
-            return false;
-        }
-        return CsiracApplication.sessionList.keySet().contains(sessionId);
-    }
+	public static boolean sessionExists(String sessionId) {
+		if (StringUtils.isBlank(sessionId)) {
+			return false;
+		}
+		return CsiracApplication.sessionList.keySet().contains(sessionId);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CsiracApplication.class, args);
