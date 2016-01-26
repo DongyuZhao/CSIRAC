bb9458691bd1e5b8113cfaf9d100166bad789383
Tessa1994 <cathyorcallmexuan@gmail.com>
2016-01-26 20:23:59 +1100

    add the commands of source code in emulator core and emulator framework.

--- src/main/java/project/csirac/website/models/document/DocumentDao.java
+++ src/main/java/project/csirac/website/models/document/DocumentDao.java
@@ -15,6 +15,6 @@ public class DocumentDao {
      * @return The document entity.
      */
     public Document findByName(String name) {
-        return new Document(name, "How to use CSIRAC emulator in this web application. Step 1:According the guide from <a href=\"/Use_Guide/user_guide.pdf\">User Guide</a>, write down the instructions in the first blank and submit the program. Step 2: Input the data and submit. Step 3: When the monitor status shows 'Ready', Submit the frequency and Start the submitted program. If you want to pause at the process of the running program, press 'Pause' and revise the PCRegister if you want. Step 4: Continue the progress. Read the program memory history, register history and data memory history from the web.", new Date());
+        return new Document(name, "How to use CSIRAC emulator in this web application. Step 1:According the guide from <a href=\"/Use_Guide/user_guide.pdf\">User Guide</a>, write down the instructions in the first blank and submit the program. Step 2: Input the data and submit. Step 3: When the monitor status shows 'Ready', Submit the frequency and Start the submitted program. If you want to pause at the process of the running program, press 'Pause' and revise the PCRegister if you want. Step 4: Continue the progress. Read the program memory history, register history and data memory history from the web. Step 5: stop the executing.", new Date());
     }
 }
